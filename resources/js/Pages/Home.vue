d6ee6b1aabc12d2b1d3d5abbbbd10c025bf4c709
I Gusti Ngurah Cipta Dwipajaya <gusti.dwipajaya@digits.id>
2023-07-01 20:42:01 +0800

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+});
+</script>
+
+<template>
+    <Head title="Mount Agung | Home" />
+
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-100"
+    >
+        <div
+            v-if="canLogin"
+            class="sm:fixed sm:top-0 sm:right-0 p-6 text-right"
+        >
+            <Link
+                v-if="$page.props.auth.user"
+                :href="route('dashboard')"
+                class="font-semibold text-gray-800"
+                >Dashboard</Link
+            >
+
+            <template v-else>
+                <Link
+                    :href="route('login')"
+                    class="font-semibold text-gray-900"
+                >
+                    Log in
+                </Link>
+
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="ml-4 font-semibold text-gray-900"
+                >
+                    Register
+                </Link>
+            </template>
+        </div>
+
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <h1 class="text-gray-900">Frontend Coming Soon</h1>
+        </div>
+    </div>
+</template>
+
+<style></style>
